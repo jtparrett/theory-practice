19d30734726c3e0e8f1fed22a708e3cbbd0395fa
jtparrett <jtparrett@me.com>
2017-09-08 13:43:04 +0100

    question setup

--- src/views/test/index.js
+++ src/views/test/index.js
@@ -1,11 +1,33 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 
+import Questions from './questions'
+
 export default class extends React.Component {
+  state = {
+    index: 0
+  }
+
+  next = () => {
+    this.setState(state => ({
+      index: ++state.index % Questions.length
+    }))
+  }
+
   render() {
+    const question = Questions[this.state.index]
     return (
       <div>
         <h2>Test</h2>
+        <h3>Question: { question.text }</h3>
+        { question.answers.map((a, i) => (
+          <div key={i}>
+            <input type="checkbox" id={i} />
+            <label htmlFor={i}>{a}</label>
+          </div>
+        )) }
+
+        <button onClick={this.next}>Next</button>
       </div>
     )
   }
---
+++ src/views/test/questions.js
@@ -0,0 +1,20 @@
+export default [{
+  text: 'You are going along a street with parked vehicles on the left-hand side. For which THREE reasons should you keep your speed down?',
+  correct: [0, 2, 3],
+  answers: [
+    'Children may run out from between the vehicles',
+    'So that oncoming traffic can see you more clearly',
+    'Vehicles may be pulling out',
+    "Drivers' doors may open",
+    'You may set off car alarms'
+  ]
+},{
+  text: 'Highways Agency Traffic Officers',
+  correct: [3],
+  answers: [
+    'are not able to stop and direct anyone on a motorway',
+    'will not be able to assist at a breakdown or emergency',
+    "will tow a broken down vehicle and it's passengers home",
+    'are able to stop and direct anyone on a motorway'
+  ]
+}]
