3b5b0b34314dcbea99d69ba4a1633efcb05429fa
jtparrett <jtparrett@me.com>
2017-09-09 07:27:15 +0100

    nav styling

--- src/views/shared/header.js
+++ src/views/shared/header.js
@@ -1,14 +1,12 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
+import {NavLink} from 'react-router-dom'
 
 export default () => (
   <header className="header">
     <h1 className="header__title">Theory Practice Online</h1>
-    <nav>
-      <ul className="header__list">
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/about">About</Link></li>
-      </ul>
+    <nav className="nav">
+      <NavLink to="/" className="nav__link" exact activeClassName="nav__link--active">Home</NavLink>
+      <NavLink to="/about" className="nav__link" activeClassName="nav__link--active">About</NavLink>
     </nav>
   </header>
 )
